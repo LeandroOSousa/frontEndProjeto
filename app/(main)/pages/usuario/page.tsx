f99f69e5b644fe4fbd0778e7cff16d5771e61736
LeandroSousa <106622352+LeandroOSousa@users.noreply.github.com>
2024-09-30 08:06:57 -0300

    CRUD perfil e correção de bugs

--- app/(main)/pages/usuario/page.tsx
+++ app/(main)/pages/usuario/page.tsx
@@ -23,7 +23,7 @@ const Usuario = () => {
         email: ''
     };
 
-    const [usuarios, setUsuarios] = useState<Projeto.Usuario[]>([]);
+    const [usuarios, setUsuarios] = useState<Projeto.Usuario[] | null>([]);
     const [usuarioDialog, setUsuarioDialog] = useState(false);
     const [deleteUsuarioDialog, setDeleteUsuarioDialog] = useState(false);
     const [deleteUsuariosDialog, setDeleteUsuariosDialog] = useState(false);
@@ -36,7 +36,7 @@ const Usuario = () => {
     const usuarioService = useMemo(() => new UsuarioService(), []);
 
     useEffect(() => {
-        if (usuarios.length == 0) {
+        if (!usuarios) {
             usuarioService.listarTodos()
                 .then((response) => {
                     console.log(response.data);
@@ -74,7 +74,7 @@ const Usuario = () => {
                 .then((response) => {
                     setUsuarioDialog(false);
                     setUsuario(usuarioVazio);
-                    setUsuarios([]);
+                    setUsuarios(null);
                     toast.current?.show({
                         severity: 'info',
                         summary: 'Sucesso!',
@@ -93,7 +93,7 @@ const Usuario = () => {
                 .then((response) => {
                     setUsuarioDialog(false);
                     setUsuario(usuarioVazio);
-                    setUsuarios([]);
+                    setUsuarios(null);
                     toast.current?.show({
                         severity: 'info',
                         summary: 'Sucesso!',
@@ -125,7 +125,7 @@ const Usuario = () => {
             usuarioService.excluir(usuario.id).then((response) => {
                 setUsuario(usuarioVazio);
                 setDeleteUsuarioDialog(false);
-                setUsuarios([]);
+                setUsuarios(null);
                 toast.current?.show({
                     severity: 'success',
                     summary: 'Sucesso!',
@@ -157,7 +157,7 @@ const Usuario = () => {
                 await usuarioService.excluir(_usuario.id);
             }
         })).then((response) => {
-            setUsuarios([]);
+            setUsuarios(null);
             setSelectedUsuarios([]);
             setDeleteUsuariosDialog(false);
             toast.current?.show({
